d60714461e9088cd81975a3f16732a7bc38bd81d
spook-QuQ <spook-QuQ@users.noreply.github.com>
2022-08-02 08:00:26 +0000

    deploy: a738e22d5a24fc77c63ee1ff4b834e1d50e221ee

--- _nuxt/static/1659426457/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftest-for-microCMS\u002F_nuxt\u002Fstatic\u002F1659426457",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Ftest-for-microCMS\u002F",assetsPath:"\u002Ftest-for-microCMS\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1659427223/articles/1drz3ujhmo/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftest-for-microCMS\u002F_nuxt\u002Fstatic\u002F1659427223",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002F1drz3ujhmo",config:{_app:{basePath:"\u002Ftest-for-microCMS\u002F",assetsPath:"\u002Ftest-for-microCMS\u002F_nuxt\u002F",cdnURL:a}}}}(null));
