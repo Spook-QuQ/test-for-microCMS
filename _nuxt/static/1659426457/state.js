0dd859d9733425f95a45c0bfe06c7e520f86a9a4
spook-QuQ <spook-QuQ@users.noreply.github.com>
2022-08-02 07:47:40 +0000

    deploy: 34a22f7ebf1ca07c89fd4a3b97847e6dab122a64

---
+++ _nuxt/static/1659426457/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftest-for-microCMS\u002F_nuxt\u002Fstatic\u002F1659426457",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftest-for-microCMS\u002F",assetsPath:"\u002Ftest-for-microCMS\u002F_nuxt\u002F",cdnURL:a}}}}(null));
